427d9f17a2ff40c1a1b5d5b20a906769ac7dbf1e
engmahmoudhelal2025 <engmahmoudhelal2025@gmail.com>
2025-04-19 07:02:49 -0700

    Upload public folder files
    Upload public folder files

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUvtSyXPND1q29rwXr0QWONJPLpj0JIuQ",
+  authDomain: "lionking-c06fb.firebaseapp.com",
+  projectId: "lionking-c06fb",
+  storageBucket: "lionking-c06fb.appspot.com",
+  messagingSenderId: "219874685776",
+  appId: "1:219874685776:web:47c6f9417ca79dfc07b255",
+  measurementId: "G-S6RG5SCLC5"
+};
